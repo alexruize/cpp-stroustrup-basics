b9568c263c5cfcedb7ec38e22eae911dd8c82773
Alex Ruiz E. <ruiz.euler@gmail.com>
2026-02-26 14:42:31 -0600

    delete cmake stuff

--- helloworld-cmake/out/build/GCC 15.2.1 x86_64-pc-linux-gnu/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/monorhesus/Documents/cpp-learn/helloworld
-# Build directory: /home/monorhesus/Documents/cpp-learn/helloworld/out/build/GCC 15.2.1 x86_64-pc-linux-gnu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
